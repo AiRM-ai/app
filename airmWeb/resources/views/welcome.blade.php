308cb361b719573dadd4fd9bd476b17839e7f756
tus13561-bhusu <tus13561@temple.edu>
2025-10-20 16:15:56 +0900

    Initialized Tailwind setup, ready to start making the website

--- airmWeb/resources/views/welcome.blade.php
+++ airmWeb/resources/views/welcome.blade.php
@@ -1,6 +1,16 @@
-<html>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-    <h1> HELLO WORLD!!</h1>
-
+        @viteReactRefresh
+        @vite(["resources/js/app.tsx", "resources/css/app.css"])
+    </head>
+    <body class="font-sans antialiased">
+        
+        <h1 class="text-4xl hover:underline bg-color-400 text-blue-400"> HELLO WORLD!! </h1>
+    </body>
 </html>
