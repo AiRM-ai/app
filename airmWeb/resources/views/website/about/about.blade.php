b14e281c7414125358716e46115c26c6c0aa9684
Komiyaki <kevinbeutler2003@gmail.com>
2025-11-10 20:44:17 +0900

    Check if its working please
    Im just commiting to main because commiting to my branch is fucking things up

--- airmWeb/resources/views/website/about/about.blade.php
+++ airmWeb/resources/views/website/about/about.blade.php
@@ -1,30 +1,3 @@
-<<<<<<< Updated upstream
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AiRM.ai</title>
-    @viteReactRefresh
-    @vite(["resources/js/app.tsx", "resources/css/app.css"])
-</head>
-<body>
-    <header>
-        <h1>About the company</h1>
-    </header>
-    <main>
-        <section>
-            <h2>We strive to make a better way for businesses to order and allocate prices based on the market as a whole</h2>
-            <h1>Our values</h1>
-            <p>We as a company love the idea of slavery and exploitation of free labor. We also believe in tax evasion and that the company values lying to the government.</p>
-        </section>
-        <section>
-            <h2>AiRM.ai Demo</h2>
-        </section>
-    </main>
-</body>
-</html>
-=======
 @extends('website.layouts.main')
 
 @section('title', 'About')
@@ -64,4 +37,3 @@
     </div>
 </main>
 @endsection
->>>>>>> Stashed changes
--- airmWeb/resources/views/website/about/tech.blade.php
+++ airmWeb/resources/views/website/about/tech.blade.php
@@ -1,26 +1,3 @@
-<<<<<<< Updated upstream
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AiRM.ai</title>
-    @viteReactRefresh
-    @vite(["resources/js/app.tsx", "resources/css/app.css"])
-</head>
-<body>
-    <header>
-        <h1>About the backend</h1>
-    </header>
-    <main>
-        <div>
-            <h2>About the models</h2>
-            <p>The currect model that keeps track of retocking and ordering dates using XGBoost.</p>
-        </div>
-    </main>
-</body>
-</html>
-=======
 @extends('website.layouts.main')
 
 @section('title', 'Technology')
@@ -71,4 +48,3 @@
     </div>
 </main>
 @endsection
->>>>>>> Stashed changes
